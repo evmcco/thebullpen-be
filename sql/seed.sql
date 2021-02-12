a2c625c1c9a28d85e9517ca6cb941864605385e9
Evan McCoy <evan.mccoy@gmail.com>
2021-02-12 09:28:30 -0500

    saving username to Auth0

--- sql/seed.sql
+++ sql/seed.sql
@@ -11,8 +11,8 @@ values
   ('evmcco', 'b', 'Tesla', 'TSLA'),
   ('evmcco', 'c', 'ARK Web ETF', 'ARKW'),
   ('evmcco', 'd', 'Gamestop', 'GME'),
-  ('evmcco', 'e', 'AMC Entertainment Holdings', 'AMC'),
-  ('evmcco', 'f', 'NIO EV CN CO', 'NIO');
+  ('vchang', 'e', 'AMC Entertainment Holdings', 'AMC'),
+  ('PhilLeotardo', 'f', 'NIO EV CN CO', 'NIO');
 
 insert into holdings
   (username, security_id, institution_price, institution_value, cost_basis, quantity)
@@ -21,6 +21,6 @@ VALUES
   ('evmcco', 'c', 171.33, 342.66, 168.43, 2),
   ('evmcco', 'f', 51.29, 153.75, 53.77, 3),
   ('evmcco', 'd', 63.20, 63.20, 233.75, 1),
-  ('evmcco', 'b', 743.32, 14863.42, 723.54, 20),
-  ('evmcco', 'e', 7.134523, 14.39834573, 8.34753475, 2.34573);
+  ('vchang', 'b', 743.32, 14863.42, 723.54, 20),
+  ('PhilLeotardo', 'e', 7.134523, 14.39834573, 8.34753475, 2.34573);
 
