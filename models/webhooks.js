b805120cf35b13ecf3d2a38c2a5f26a215bd170e
Evan McCoy <evan.mccoy@gmail.com>
2021-02-23 09:06:35 -0500

    attempt 3

--- models/webhooks.js
+++ models/webhooks.js
@@ -6,8 +6,8 @@ class Webhooks {
   static async saveWebhook(webhook) {
     const datetime = moment().format()
     try {
-      const response = await db.any('insert into webhooks (plaid_webhook, date_sent) values ($1, $2)', webhook, datetime);
-      return response;
+      const response = await db.one('insert into webhooks (plaid_webhook, date_sent) values ($1, $2)', [webhook, datetime]);
+      return true;
     } catch (err) {
       return err.message;
     }
