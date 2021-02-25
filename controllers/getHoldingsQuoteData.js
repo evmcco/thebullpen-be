22d0523abeb29a5255a503313203a520352d9b39
Evan McCoy <evan.mccoy@gmail.com>
2021-02-25 09:00:00 -0500

    fetching IEX Cloud quote data with holdings

---
+++ controllers/getHoldingsQuoteData.js
@@ -0,0 +1,19 @@
+const getHoldingsQuoteData = async (holdings) => {
+  const fetch = require('node-fetch');
+  const holdingsWithQuotes = await Promise.all(holdings.map(async (holding) => {
+    //fetch IEX quote endpoint for ticker
+    try {
+      const response = await fetch(`https://cloud.iexapis.com/stable/stock/${holding.ticker_symbol}/quote?token=pk_e633719ef3e74b81914e51099c7b012d`)
+      const quote = await response.json()
+      //add response to holding object
+      return { ...holding, quote }
+    }
+    catch (err) {
+      console.log(err)
+      return { ...holding }
+    }
+  }))
+  return holdingsWithQuotes
+}
+
+module.exports = { getHoldingsQuoteData }
--- routes/holdings.js
+++ routes/holdings.js
@@ -2,9 +2,13 @@ const express = require("express");
 const router = express.Router();
 const holdingsModel = require("../models/holdings");
 
+const { getHoldingsQuoteData } = require("../controllers/getHoldingsQuoteData")
+
 router.get("/user/:username", async (req, res, next) => {
   const userHoldings = await holdingsModel.getHoldingsByUser(req.params.username);
-  res.json(userHoldings).status(200);
+  //take tickers from userHoldings, hit IEX with each of them, add IEX data to each holdings object, then send to FE
+  const userHoldingsWithQuotes = await getHoldingsQuoteData(userHoldings)
+  res.json(userHoldingsWithQuotes).status(200);
 });
 
 router.post("/save", async (req, res, next) => {
