20153b7ace5834c120c982bfb4339a805952f37e
Evan McCoy <evan.mccoy@gmail.com>
2021-03-02 21:16:29 -0500

    finalizing options parsing

--- routes/holdings.js
+++ routes/holdings.js
@@ -10,15 +10,13 @@ router.get("/user/:username", async (req, res, next) => {
   const derivatives = userHoldings.filter((holding) => {
     return holding.type == 'derivative'
   })
-  console.log("DERIVATIVES", derivatives)
   const equities = userHoldings.filter((holding => {
     return holding.type != 'derivative'
   }))
-  console.log("EQUITIES", equities)
   //take tickers from userHoldings, hit IEX with each of them, add IEX data to each holdings object, then send to FE
   const userEquitiesHoldingsWithQuotes = await getHoldingsQuoteData(equities)
   const parsedDerivativesWithQuotes = await processDerivatives(derivatives)
-  res.json({ userEquitiesHoldingsWithQuotes, parsedDerivativesWithQuotes }).status(200);
+  res.json([...userEquitiesHoldingsWithQuotes, ...parsedDerivativesWithQuotes]).status(200);
 });
 
 router.post("/save", async (req, res, next) => {
