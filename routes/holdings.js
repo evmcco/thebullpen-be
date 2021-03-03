72b7bf83cec00fe43230c4836d76e456ce3e5dec
Evan McCoy <evan.mccoy@gmail.com>
2021-03-02 20:37:58 -0500

    parsing derivatives on the backend, getting quote data too

--- routes/holdings.js
+++ routes/holdings.js
@@ -3,12 +3,22 @@ const router = express.Router();
 const holdingsModel = require("../models/holdings");
 
 const { getHoldingsQuoteData } = require("../controllers/getHoldingsQuoteData")
+const { processDerivatives } = require("../controllers/processDerivatives")
 
 router.get("/user/:username", async (req, res, next) => {
   const userHoldings = await holdingsModel.getHoldingsByUser(req.params.username);
+  const derivatives = userHoldings.filter((holding) => {
+    return holding.type == 'derivative'
+  })
+  console.log("DERIVATIVES", derivatives)
+  const equities = userHoldings.filter((holding => {
+    return holding.type != 'derivative'
+  }))
+  console.log("EQUITIES", equities)
   //take tickers from userHoldings, hit IEX with each of them, add IEX data to each holdings object, then send to FE
-  const userHoldingsWithQuotes = await getHoldingsQuoteData(userHoldings)
-  res.json(userHoldingsWithQuotes).status(200);
+  const userEquitiesHoldingsWithQuotes = await getHoldingsQuoteData(equities)
+  const parsedDerivativesWithQuotes = await processDerivatives(derivatives)
+  res.json({ userEquitiesHoldingsWithQuotes, parsedDerivativesWithQuotes }).status(200);
 });
 
 router.post("/save", async (req, res, next) => {
