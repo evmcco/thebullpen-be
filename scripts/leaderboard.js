3fffc229be2730d7e21ed6017e29d94e2cb83e6d
Evan McCoy <evan.mccoy@gmail.com>
2021-05-02 10:41:57 -0400

    fixing leaderboard script weekend skip

--- scripts/leaderboard.js
+++ scripts/leaderboard.js
@@ -6,12 +6,15 @@ const { calculateDailyPerformance } = require("../controllers/calculateDailyPerf
 
 const leaderboard = async () => {
   const dayOfWeek = moment().day()
-  if (dayOfWeek === 6 || dayOfWeek === 7) {
+  console.log(`day of week: ${dayOfWeek}`)
+  if (dayOfWeek === 0 || dayOfWeek === 6) {
     console.log("skipping leaderboard calculation, its the weekend")
   } else {
+    console.log("weekday detected, running calculation")
     const allUsers = await bullpensModel.getAllBullpens()
     allUsers.forEach(async (user) => {
       const userPerformance = await calculateDailyPerformance(user.username)
+      console.log(`saving performance for ${user.username}: ${userPerformance}`)
       performanceModel.saveTickerPerformance(user.username, userPerformance)
     })
   }
