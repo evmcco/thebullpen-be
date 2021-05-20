72d7e2ec6ff5eab6715f45338aae54b44662bc91
Eric Mauldin <emauldin84@gmail.com>
2021-05-20 12:10:17 -0400

    replaces dashes with underscores in follows routes.

--- routes/follows.js
+++ routes/follows.js
@@ -7,7 +7,7 @@ const {
   getAllUserFollowers
 } = require("../controllers/follows");
 
-router.post("/add-follow", async (req, res, next) => {
+router.post("/add_follow", async (req, res, next) => {
   const follower = req.body.followerUsername
   const followee = req.body.followeeUsername
   const addedFollow = await addFollow(follower, followee)
@@ -15,21 +15,21 @@ router.post("/add-follow", async (req, res, next) => {
   res.json(addedFollow)
 })
 
-router.delete("/remove-follow", async (req, res, next) => {
+router.delete("/remove_follow", async (req, res, next) => {
   const followerId = req.body.followId
   const removedFollow = await removeFollow(followerId)
   console.log(removedFollow)
   res.json(removedFollow)
 })
 
-router.get("/user-follows", async (req, res, next) => {
+router.post("/user_follows", async (req, res, next) => {
   const username = req.body.username
   const follows = await getAllUserFollows(username)
   console.log('follows', follows)
   res.json(follows)
 })
 
-router.get("/user-followers", async (req, res, next) => {
+router.post("/user_followers", async (req, res, next) => {
   const username = req.body.username
   const followers = await getAllUserFollowers(username)
   console.log('followers', followers)
