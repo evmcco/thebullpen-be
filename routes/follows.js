930cb5885daa9bd5437667a597299026c4f3c5cf
Eric Mauldin <emauldin84@gmail.com>
2021-05-21 16:26:49 -0400

    removes console.logs

--- routes/follows.js
+++ routes/follows.js
@@ -11,28 +11,24 @@ router.post("/add_follow", async (req, res, next) => {
   const follower = req.body.followerUsername
   const followee = req.body.followeeUsername
   const addedFollow = await addFollow(follower, followee)
-  console.log(addedFollow)
   res.json(addedFollow)
 })
 
 router.delete("/remove_follow", async (req, res, next) => {
   const followerId = req.body.followId
   const removedFollow = await removeFollow(followerId)
-  console.log(removedFollow)
   res.json(removedFollow)
 })
 
 router.post("/user_follows", async (req, res, next) => {
   const username = req.body.username
   const follows = await getAllUserFollows(username)
-  console.log('follows', follows)
   res.json(follows)
 })
 
 router.post("/user_followers", async (req, res, next) => {
   const username = req.body.username
   const followers = await getAllUserFollowers(username)
-  console.log('followers', followers)
   res.json(followers)
 })
 
