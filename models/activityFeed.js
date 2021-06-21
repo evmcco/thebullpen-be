51db813efa37d04c02cea9713c1de078420857c6
Eric Mauldin <emauldin84@gmail.com>
2021-06-21 16:30:56 -0400

    removes unused pagenumber code and fetches activiy from the db based on activity id.

--- models/activityFeed.js
+++ models/activityFeed.js
@@ -3,10 +3,13 @@ const moment = require('moment');
 
 class ActivityFeed {
   static async addPlaidTransactions(transactions) {
+    let sortedTransactions = transactions.sort((a, b)=>
+      new Date(a.date) - new Date(b.date)
+    )
     const datetime = moment().format()
     await db.tx(t => {
       try {
-      const queries = transactions.map(trans => {
+      const queries = sortedTransactions.map(trans => {
         let text = `On ${moment(trans.date).format('ddd MMM Do YYYY')} ${trans.username} ${trans.subtype === 'buy' ? 'bought': 'sold'} ${trans.ticker ? trans.ticker : trans.ticker_symbol} at $${Number(trans.price).toFixed(2)}`
         return t.none('INSERT INTO activity_feed (date, username, activity_type, activity, investment_transaction_id) VALUES(($1), ($2), ($3), ($4), ($5)) on conflict (investment_transaction_id) do nothing',
         [datetime, trans.username, 'transaction', `{"date": "${trans.date}", "asset": "${trans.ticker ? trans.ticker : trans.ticker_symbol}", "type": "${trans.subtype}", "price": "${trans.price}", "text": "${text}", "ticker": "${trans.ticker_symbol}"}`, trans.investment_transaction_id])
@@ -26,9 +29,14 @@ class ActivityFeed {
     })
   }
 
-  static async getUserActivityFeed(username) {
+  static async getUserActivityFeed(username, lastActivityId) {
+    let limit = 10
     try {
-      const response = await db.any('WITH user_follows AS (SELECT follow_id, follower_username, followee_username FROM follows where follower_username = ($1)) SELECT * from activity_feed a join user_follows f on a.username = f.followee_username order by a.date desc', [username]);
+      let {max} = await db.one("SELECT max(id) from activity_feed")
+      lastActivityId = lastActivityId > 0 ? lastActivityId - 1 : max
+      // const response = await db.any("WITH user_follows AS (SELECT follow_id, follower_username, followee_username FROM follows where follower_username = ($1)) SELECT * from activity_feed a join user_follows f on a.username = f.followee_username WHERE a.id > ($4) order by a.activity -> to_char(date, 'YYYY-MM-DD') desc LIMIT ($3)", [username, offset, limit, lastActivityId]);
+      const response = await db.any("WITH user_follows AS (SELECT follow_id, follower_username, followee_username FROM follows where follower_username = ($1)) SELECT * from activity_feed a join user_follows f on a.username = f.followee_username WHERE a.id <= ($3) order by a.id desc LIMIT ($2)", [username, limit, lastActivityId]);
+
       return response;
     } catch (err) {
       console.log('error: ', err)
--- routes/activityFeed.js
+++ routes/activityFeed.js
@@ -2,9 +2,10 @@ const express = require("express");
 const router = express.Router();
 const ActivityFeedModel = require("../models/activityFeed");
 
-router.post("/get_user_activity_feed", async (req, res, next) => {
-  let username = req.body.username
-  const response = await ActivityFeedModel.getUserActivityFeed(username);
+router.get("/get_user_activity_feed", async (req, res, next) => {
+  let username = req.query.username
+  let lastActivityId = req.query.lastActivityId
+  const response = await ActivityFeedModel.getUserActivityFeed(username, lastActivityId);
 
   res.json(response).status(200);
 });
